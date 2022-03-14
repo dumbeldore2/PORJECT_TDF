7751f4d1b90f46a2757bb50678599c054dc21edd
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2022-03-14 11:59:36 +0100

    alle tops en bots zijn aangepast voor de 3 niewe mas

--- app/src/main/java/com/example/porject_tdf/MainActivity6.java
+++ app/src/main/java/com/example/porject_tdf/MainActivity6.java
@@ -3,6 +3,7 @@ package com.example.porject_tdf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity6 extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+        getWindow().setNavigationBarColor(getResources().getColor(R.color.c1));
+        getWindow().setStatusBarColor(getResources().getColor(R.color.c1));
     }
 }
