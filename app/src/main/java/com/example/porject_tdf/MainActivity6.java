5a3ae7d2eb611c00474b31a7a222059bca08e6da
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2022-03-14 20:31:30 +0100

    een connectie gemaakt tussen ma 1 en ma 6 en ma 6 en ma 5

--- app/src/main/java/com/example/porject_tdf/MainActivity6.java
+++ app/src/main/java/com/example/porject_tdf/MainActivity6.java
@@ -2,10 +2,16 @@ package com.example.porject_tdf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity6 extends AppCompatActivity {
+
+    //imageviews initen
+    ImageView image1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,20 @@ public class MainActivity6 extends AppCompatActivity {
         );
         getWindow().setNavigationBarColor(getResources().getColor(R.color.c1));
         getWindow().setStatusBarColor(getResources().getColor(R.color.c1));
+
+        //imageses conecten
+        image1 = findViewById(R.id.image_1);
+
+        //functies
+        click_1();
+    }
+
+    public void click_1(){
+        image1.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity5.class);
+                startActivity(intent);
+            }
+        });
     }
 }
