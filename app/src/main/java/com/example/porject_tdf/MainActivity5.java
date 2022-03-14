5837abab6b1eb8b45a07e176932198e56b256f3f
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2022-03-15 00:49:35 +0100

    backbut van uit inlog is gemaakt en gelinkt

--- app/src/main/java/com/example/porject_tdf/MainActivity5.java
+++ app/src/main/java/com/example/porject_tdf/MainActivity5.java
@@ -2,10 +2,17 @@ package com.example.porject_tdf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity5 extends AppCompatActivity {
+
+    //images initen
+    ImageView image1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +24,20 @@ public class MainActivity5 extends AppCompatActivity {
         );
         getWindow().setNavigationBarColor(getResources().getColor(R.color.c1));
         getWindow().setStatusBarColor(getResources().getColor(R.color.c1));
+
+        //images conectyen
+        image1 = findViewById(R.id.image_1);
+
+        //function
+        click_1();
+    }
+
+    public void click_1(){
+        image1.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity6.class);
+                startActivity(intent);
+            }
+        });
     }
 }
