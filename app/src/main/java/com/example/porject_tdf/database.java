61e047804a3b6c8082fdcfdc61c50533abb6f6cd
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2022-03-18 00:58:24 +0100

    niewe tabel is toegevoegd samen met de idmaker en de default add to table

--- app/src/main/java/com/example/porject_tdf/database.java
+++ app/src/main/java/com/example/porject_tdf/database.java
@@ -27,6 +27,17 @@ public class database extends SQLiteOpenHelper {
     public static final String Table_2_col_1 = "inlog_log_naam";
     public static final String Table_2_col_2 = "inlog_log_status";
 
+    //tabel 3
+    public static final String DATABASE_table_3 = "c";
+    public static final String Table_3_col_0 = "c_id";
+    public static final String Table_3_col_1 = "c_naam";
+    public static final String Table_3_col_2 = "c_status";
+    public static final String Table_3_col_3 = "c_bol";
+    public static final String Table_3_col_4 = "c_wit";
+    public static final String Table_3_col_5 = "c_groen";
+    public static final String Table_3_col_6 = "c_geel";
+    public static final String Table_3_col_7 = "c_field";
+
 
     public database(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -41,12 +52,18 @@ public class database extends SQLiteOpenHelper {
         db.execSQL("create table " + DATABASE_table_2 + "(" + Table_2_col_0 + " INTEGER DEFAULT 0" +
                 " primary key ,"
                 + Table_2_col_1 + " TEXT default 'def'," + Table_2_col_2 + " TEXT default 'def')");
+
+        db.execSQL("create table " + DATABASE_table_3 + "(" + Table_3_col_0 + " INTEGER DEFAULT 0" +
+                " primary key ,"
+                + Table_3_col_1 + " TEXT default 'def'," + Table_3_col_2 + " TEXT default 'def'," + Table_3_col_3 +
+                "  TEXT default 'def'," + Table_3_col_4 + " TEXT default 'def'," + Table_3_col_5 + " TEXT default 'def'," + Table_3_col_6 + " TEXT default 'def'," + Table_3_col_7 + " TEXT default 'def')");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + DATABASE_table_1);
         db.execSQL("DROP TABLE IF EXISTS " + DATABASE_table_2);
+        db.execSQL("DROP TABLE IF EXISTS " + DATABASE_table_3);
     }
 
 
@@ -70,7 +87,6 @@ public class database extends SQLiteOpenHelper {
         }
         return uit;
     }
-
     public void addToTabel1(String s1,String s2,String s3,String s4) {
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
@@ -83,7 +99,6 @@ public class database extends SQLiteOpenHelper {
 
         sqLiteDatabase.insert(DATABASE_table_1, null, contentValues);
     }
-
     public String[] s1() {
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         Cursor cursor = sqLiteDatabase.rawQuery("select " + Table_1_col_1 + " from " + DATABASE_table_1 + "", null);
@@ -113,7 +128,6 @@ public class database extends SQLiteOpenHelper {
         }
         return uits;
     }
-
     public int length_table_1(){
         int uit = 0;
 
@@ -125,9 +139,6 @@ public class database extends SQLiteOpenHelper {
         uit = cursor.getCount();
         return uit;
     }
-
-
-
 
     //table 2
     public int IDMAKER_TABLE_2(){
@@ -150,7 +161,6 @@ public class database extends SQLiteOpenHelper {
         }
         return uit;
     }
-
     public void addToTabel2(String s1,String s2) {
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
@@ -161,7 +171,6 @@ public class database extends SQLiteOpenHelper {
 
         sqLiteDatabase.insert(DATABASE_table_2, null, contentValues);
     }
-
     public int length_table_2(){
         int uit = 0;
 
@@ -173,7 +182,6 @@ public class database extends SQLiteOpenHelper {
         uit = cursor.getCount();
         return uit;
     }
-
     public String lastStatus(){
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         Cursor cursor =
@@ -188,5 +196,37 @@ public class database extends SQLiteOpenHelper {
             }
         }
         return uit;
+    }
+
+    //table 3
+    public int IDMAKER_TABLE_3(){
+        int uit = -1;
+
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery(
+                "select * from " + DATABASE_table_3, null
+        );
+        if (cursor.getCount() == 0) {
+            uit = 0;
+        } else {
+            Cursor cursor1 =
+                    sqLiteDatabase.rawQuery("select max(" + Table_3_col_0 + " ) from " + DATABASE_table_3 + "", null);
+            StringBuffer stringBuffer = new StringBuffer();
+            if (cursor1.moveToFirst()) {
+                stringBuffer.append(cursor1.getString(0));
+                uit = Integer.parseInt(stringBuffer.toString()) + 1;
+            }
+        }
+        return uit;
+    }
+    public void addToTabel3(String s1,String s2) {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(Table_3_col_0, IDMAKER_TABLE_2());
+        contentValues.put(Table_3_col_1, s1);
+        contentValues.put(Table_3_col_7, s2);
+
+        sqLiteDatabase.insert(DATABASE_table_2, null, contentValues);
     }
 }
