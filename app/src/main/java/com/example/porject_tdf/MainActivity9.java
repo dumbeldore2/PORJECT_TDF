43a623ba99f638a54de0dc544cb7a1485c8d08da
PC001 laptop <yago.engels@gmail.com>
2022-03-25 22:39:59 +0100

    MainActivity9 aangepast

--- app/src/main/java/com/example/porject_tdf/MainActivity9.java
+++ app/src/main/java/com/example/porject_tdf/MainActivity9.java
@@ -2,10 +2,16 @@ package com.example.porject_tdf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity9 extends AppCompatActivity {
+
+    //textviews initen
+    TextView text2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,23 @@ public class MainActivity9 extends AppCompatActivity {
         );
         getWindow().setNavigationBarColor(getResources().getColor(R.color.c1));
         getWindow().setStatusBarColor(getResources().getColor(R.color.c1));
+
+        //text connecten
+        text2 = findViewById(R.id.text_2);
+
+        //functions
+        click_1();
+    }
+
+
+    public void click_1() {
+        text2.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity8.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
