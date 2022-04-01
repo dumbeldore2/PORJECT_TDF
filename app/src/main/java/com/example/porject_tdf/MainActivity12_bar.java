a1bd924eeae8cb2b3614672bbb2b6d6168a1ec68
PC001 laptop <yago.engels@gmail.com>
2022-04-01 22:42:32 +0200

    bar activity toegevoegd

---
+++ app/src/main/java/com/example/porject_tdf/MainActivity12_bar.java
@@ -0,0 +1,14 @@
+package com.example.porject_tdf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity12_bar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity12_bar);
+    }
+}
