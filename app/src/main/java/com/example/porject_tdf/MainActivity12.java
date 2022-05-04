1629efaaf88aa1f8cff3d6090daed579f5da4d46
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2022-05-05 00:43:21 +0200

    v1.28 3

--- app/src/main/java/com/example/porject_tdf/MainActivity12.java
+++ app/src/main/java/com/example/porject_tdf/MainActivity12.java
@@ -2,10 +2,18 @@ package com.example.porject_tdf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity12 extends AppCompatActivity {
+
+    //image views initen
+    ImageView imageView1;
+
+    //database initen
+    database db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +25,22 @@ public class MainActivity12 extends AppCompatActivity {
         );
         getWindow().setNavigationBarColor(getResources().getColor(R.color.c1));
         getWindow().setStatusBarColor(getResources().getColor(R.color.c1));
+
+        //imageviews conecten
+        imageView1 = findViewById(R.id.image_1);
+
+        //database conecten
+        db = new database(this);
+
+        //functies
+        click_1();
     }
+
+    public void click_1(){
+        Intent intent = new Intent(getApplicationContext(),MainActivity13.class);
+        db.addToTabel4("adding yellow info", db.getTable_4_col_3());
+        startActivity(intent);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/porject_tdf/MainActivity13.java
@@ -0,0 +1,21 @@
+package com.example.porject_tdf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity13 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main13);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+        getWindow().setNavigationBarColor(getResources().getColor(R.color.c1));
+        getWindow().setStatusBarColor(getResources().getColor(R.color.c1));
+    }
+}
